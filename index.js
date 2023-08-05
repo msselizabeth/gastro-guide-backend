330813f63bb41292b6166a88fee7c21f4e204026
msselizabeth <lizagotovitryby@gmail.com>
2023-08-05 13:46:31 +0300

    starter settings for backend

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const continentsRouter = require("./routes/api/continents")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/continents", continentsRouter);
+
+
+
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Not found",
+    })
+})
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({
+        message,
+    })
+})
+
+
+module.exports = app;
+// app.listen(3003, () => { console.log("server running") });
---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./index");
+const mongoose = require("mongoose");
+
+const DB_HOST = 'mongodb+srv://msselizabeth:EliSher090398GastroGuide@gastroguide.9ja7kgx.mongodb.net/gastroguide?retryWrites=true&w=majority'
+
+mongoose.connect(DB_HOST)
+    .then(() => {
+        app.listen(3003)
+        console.log("Connect success")
+    })
+    .catch(error => {
+        console.log(error.message)
+        process.exit(1);
+    })
+;
