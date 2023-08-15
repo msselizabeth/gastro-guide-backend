42bedcd0a71d0fe46d8a01a06d2cdd4c39e3577d
msselizabeth <lizagotovitryby@gmail.com>
2023-08-15 16:41:04 +0300

    controller recipes list

--- controllers/getCountryRecipes.js
+++ controllers/getCountryRecipes.js
@@ -2,7 +2,7 @@ const { Recipe } = require("../models/recipe");
 
 const getCountryRecipes = async (req, res) => {
     const { countryId: owner } = req.params;
-    const result = await Recipe.find({owner});
+    const result = await Recipe.find({owner}, { recipeName: 1, owner: 1, recipeImgSmall: 1, _id: 1 });
     res.json(result);
 
 }
