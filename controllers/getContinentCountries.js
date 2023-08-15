9ec3efc30f9b814f1ee581162c9c75e61c326fcc
msselizabeth <lizagotovitryby@gmail.com>
2023-08-15 16:07:17 +0300

    fix find countries

--- controllers/getContinentCountries.js
+++ controllers/getContinentCountries.js
@@ -3,7 +3,7 @@ const { Country } = require("../models/country");
 
 const getContinentCountries  = async (req, res) => {
     const { continentId: owner } = req.params;
-    const result = await Country.find({owner});
+    const result = await Country.find({owner} , { countryName: 1, owner: 1, imageSmall: 1, _id: 1 });
     res.json(result);
 }
 
