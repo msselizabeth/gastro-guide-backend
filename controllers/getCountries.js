abd67b0e466bf879d54c0d75e335a40725d6bd05
msselizabeth <lizagotovitryby@gmail.com>
2023-08-16 21:32:57 +0300

    fix

--- controllers/getCountries.js
+++ controllers/getCountries.js
@@ -3,8 +3,14 @@ const { Country } = require("../models/country");
 const getCountries = async (req, res) => {
         const { page = 1, limit = 20} = req.query;
         const skip = (page - 1) * limit;
+         const totalCount = await Country.countDocuments(); // Общее количество стран
+        const totalPages = Math.ceil(totalCount / limit); 
         const result = await Country.find({ },'', { skip, limit });
-        res.json(result);
+        res.json({
+      totalPages,
+      currentPage: page,
+      result,
+    });
 }
 
 module.exports = getCountries;
